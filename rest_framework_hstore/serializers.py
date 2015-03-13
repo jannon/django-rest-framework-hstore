da973498aedbae4522fefe07d3dddee0c2c81ee7
Jannon Frank <jannon.frank@tellagami.com>
2015-03-13 12:55:40 -0700

    update HStoreSerializer to use new custom ModelSerializer API

--- rest_framework_hstore/serializers.py
+++ rest_framework_hstore/serializers.py
@@ -28,7 +28,7 @@ class HStoreSerializer(ModelSerializer):
         # TODO: support ReferenceField
         self.serializer_field_mapping[DictionaryField] = HStoreField
 
-    def get_field(self, model_field):
+    def build_standard_field(self, field_name, model_field):
         """
         Creates a default instance of a basic non-relational field.
         """
@@ -44,7 +44,7 @@ class HStoreSerializer(ModelSerializer):
             kwargs['default'] = model_field.get_default()
 
         if issubclass(model_field.__class__, models.TextField):
-            kwargs['widget'] = widgets.Textarea
+            kwargs['style'] = {'base_template': 'textarea.html'}
 
         if model_field.verbose_name is not None:
             kwargs['label'] = model_field.verbose_name
@@ -57,7 +57,7 @@ class HStoreSerializer(ModelSerializer):
             kwargs['choices'] = model_field.flatchoices
             if model_field.null:
                 kwargs['empty'] = None
-            return ChoiceField(**kwargs)
+            return (ChoiceField, kwargs)
 
         # put this below the ChoiceField because min_value isn't a valid initializer
         if issubclass(model_field.__class__, models.PositiveIntegerField) or\
@@ -75,52 +75,30 @@ class HStoreSerializer(ModelSerializer):
             models.URLField: ['max_length'],
         }
 
-        if model_field.__class__ in attribute_dict:
-            attributes = attribute_dict[model_field.__class__]
+        # === django-rest-framework-hstore specific ====
+        # if available, use __basefield__ attribute instead of __class__
+        # this will cause DRF to pick the correct DRF-field
+        key = getattr(model_field, '__basefield__', model_field.__class__)
+
+        if key in attribute_dict:
+            attributes = attribute_dict[key]
             for attribute in attributes:
                 kwargs.update({attribute: getattr(model_field, attribute)})
 
         if model_field.__class__ == DictionaryField and model_field.schema:
             kwargs['schema'] = True
 
-        # === django-rest-framework-hstore specific ====
-        # if available, use __basefield__ attribute instead of __class__
-        # this will cause DRF to pick the correct DRF-field
-        key = getattr(model_field, '__basefield__', model_field.__class__)
-
         try:
-            return self.field_mapping[key](**kwargs)
+            return (self.serializer_field_mapping[key], kwargs)
         except KeyError:
             pass
 
         try:
-            return self.field_mapping[model_field.__class__.__name__](**kwargs)
+            return (self.serializer_field_mapping[model_field.__class__.__name__], kwargs)
         except KeyError:
-            return ModelField(model_field=model_field, **kwargs)
+            # return ModelField(model_field=model_field, **kwargs)
+            return super(HStoreSerializer, self).build_standard_field(field_name, model_field)
 
-        # DRF <= 2.4
-#     def restore_object(self, attrs, instance=None):
-#         """
-#         temporarily remove hstore virtual fields otherwise DRF considers them many2many
-#         """
-#         model = self.opts.model
-#         meta = self.opts.model._meta
-#         original_virtual_fields = list(meta.virtual_fields)  # copy
-#
-#         if hasattr(model, '_hstore_virtual_fields'):
-#             # remove hstore virtual fields from meta
-#             for field in model._hstore_virtual_fields.values():
-#                 meta.virtual_fields.remove(field)
-#
-#         instance = super(HStoreSerializer, self).restore_object(attrs, instance)
-#
-#         if hasattr(model, '_hstore_virtual_fields'):
-#             # restore original virtual fields
-#             meta.virtual_fields = original_virtual_fields
-#
-#         return instance
-
-    # DRF >= 3.0
     def update(self, instance, validated_data):
         """
         temporarily remove hstore virtual fields otherwise DRF considers them many2many
